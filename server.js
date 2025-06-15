6bc2c19a514e06ef288db8f8a66672c75a90e7b3
pratyushswain01 <pratyushkumarswain2005@gmail.com>
2025-06-16 00:50:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io =require('socket.io')(server);
+const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname)); 
 
-app.get('/', (req, res) => {
+// This is the NEW "catch-all" route.
+// It will serve your main HTML file for ANY path, like /my-class, /another-class, etc.
+app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/VirtualClassroom.html');
 });
 
@@ -21,7 +23,7 @@ io.on('connection', socket => {
     });
 });
 
-// THIS IS THE CORRECTED LINE FOR RENDER DEPLOYMENT
+// This line is already correct for Render deployment
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Zenith Server listening on port ${PORT}`);
 });
